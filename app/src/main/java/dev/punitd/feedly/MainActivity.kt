828f22ae9857a6f70f2f12c2aae1997281f41a99
punitda <punitdama@gmail.com>
2021-09-25 17:42:11 +0530

    Import "base-android" inside "app" module

--- app/src/main/java/dev/punitd/feedly/MainActivity.kt
+++ app/src/main/java/dev/punitd/feedly/MainActivity.kt
@@ -2,11 +2,15 @@ package dev.punitd.feedly
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dev.punitd.base.android.extensions.viewBinding
+import dev.punitd.feedly.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
