c285ed284aaa60186e633bed7a7275146242264c
punitda <punitdama@gmail.com>
2021-09-26 12:27:06 +0530

    Set up bottom nav with nav_graph

--- app/src/main/java/dev/punitd/feedly/MainActivity.kt
+++ app/src/main/java/dev/punitd/feedly/MainActivity.kt
@@ -2,15 +2,36 @@ package dev.punitd.feedly
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
 import dev.punitd.base.android.extensions.viewBinding
 import dev.punitd.feedly.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val binding by viewBinding(ActivityMainBinding::inflate)
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        // Setup Bottom Nav with NavController
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_container
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        // This is to avoid reloading same fragment again when selected item is reselected.
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            if (it.itemId != binding.bottomNavigationView.selectedItemId)
+                NavigationUI.onNavDestinationSelected(it, navController)
+            true
+        }
     }
 }
