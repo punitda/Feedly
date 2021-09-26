46634d672600d45f7deecc1900e19a2b977e2c33
punitda <punitdama@gmail.com>
2021-09-26 13:32:56 +0530

    Create "data" module

---
+++ data/src/main/java/dev/punitd/data/Article.kt
@@ -0,0 +1,11 @@
+package dev.punitd.data
+
+data class Article(
+    val guid: String,
+    val title: String,
+    val author: String,
+    val link: String,
+    val pubDate: String,
+    val description: String,
+    val content: String,
+)
