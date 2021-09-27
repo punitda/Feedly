f8ccd346e878e081b3e9a9bb5ad79d02a45b37f6
punitda <punitdama@gmail.com>
2021-09-27 12:54:53 +0530

    Create Mapper to convert from ArticleDtotoArticle

---
+++ rss-parser/src/main/java/dev/punitd/rss/parser/data/mapper/ArticleDtoToArticle.kt
@@ -0,0 +1,45 @@
+package dev.punitd.rss.parser.data.mapper
+
+import com.prof.rssparser.Channel
+import dev.punitd.base.Mapper
+import dev.punitd.base.data.Error
+import dev.punitd.base.data.Result
+import dev.punitd.base.data.Success
+import javax.inject.Inject
+
+class ArticleDtoToArticle @Inject constructor() :
+    Mapper<Result<Channel>, Result<dev.punitd.data.Channel>> {
+
+    override suspend fun map(from: Result<Channel>): Result<dev.punitd.data.Channel> {
+        return when (from) {
+            is Error -> Error(from.throwable)
+            is Success -> {
+                val channel = dev.punitd.data.Channel(
+                    title = from.data.title ?: "",
+                    link = from.data.link ?: "",
+                    description = from.data.description,
+                    image = dev.punitd.data.Channel.Image(
+                        title = from.data.image?.title ?: "",
+                        url = from.data.image?.url ?: "",
+                        link = from.data.image?.link ?: "",
+                    ),
+                )
+
+                val articles = from.data.articles.map {
+                    dev.punitd.data.Article(
+                        guid = it.guid ?: "",
+                        title = it.title ?: "",
+                        author = it.author ?: "",
+                        link = it.link ?: "",
+                        pubDate = it.pubDate ?: "",
+                        description = it.description ?: "",
+                        content = it.content ?: "",
+                        imageUrl = it.image ?: "",
+                        categories = it.categories
+                    )
+                }
+                Success(data = channel.copy(articles = articles))
+            }
+        }
+    }
+}
