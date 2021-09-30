d9aa899213ab2d71058f3eafc57a1a231a70712a
punitda <punitdama@gmail.com>
2021-09-30 15:14:06 +0530

    Bind ChannelList UseCase into ViewModelComponent

--- features/feed/src/main/java/dev/punitd/features/feed/inject/FeedModule.kt
+++ features/feed/src/main/java/dev/punitd/features/feed/inject/FeedModule.kt
@@ -6,6 +6,8 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dev.punitd.rss.parser.domain.usecase.GetChannelUseCase
 import dev.punitd.rss.parser.domain.usecase.GetChannelUseCaseImpl
+import dev.punitd.rss.parser.domain.usecase.GetChannelsListUseCase
+import dev.punitd.rss.parser.domain.usecase.GetChannelsListUseCaseImpl
 
 @InstallIn(ViewModelComponent::class)
 @Module
@@ -15,4 +17,9 @@ abstract class FeedModule {
     abstract fun provideGetChannelUseCase(
         getChannelUseCase: GetChannelUseCaseImpl
     ): GetChannelUseCase
+
+    @Binds
+    abstract fun provideGetChannelsListUseCasew(
+        getChannelsListUseCase: GetChannelsListUseCaseImpl
+    ): GetChannelsListUseCase
 }
