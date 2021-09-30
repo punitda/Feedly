11eff4ae5186e8789bd39bb68369d25a69d251ff
punitda <punitdama@gmail.com>
2021-09-30 16:14:37 +0530

    Remove Categories carousel

--- features/feed/src/main/java/dev/punitd/features/feed/FeedController.kt
+++ features/feed/src/main/java/dev/punitd/features/feed/FeedController.kt
@@ -3,7 +3,6 @@ package dev.punitd.features.feed
 import com.airbnb.epoxy.TypedEpoxyController
 import dev.punitd.data.Article
 import dev.punitd.data.Channel
-import dev.punitd.ui.components.view.CategoryView_
 import dev.punitd.ui.components.view.articleItemView
 import dev.punitd.ui.components.view.channelInfoView
 import dev.punitd.ui.components.view.sectionHeader
@@ -35,11 +34,6 @@ class FeedController : TypedEpoxyController<Channel>() {
                 articleItemView {
                     id(it.guid)
                     article(it)
-                    categories(
-                        it.categories.map {
-                            CategoryView_().id(it).category(it)
-                        }
-                    )
                 }
             }
         }
--- ui-components/src/main/java/dev/punitd/ui/components/view/ArticleItemView.kt
+++ ui-components/src/main/java/dev/punitd/ui/components/view/ArticleItemView.kt
@@ -1,17 +1,14 @@
 package dev.punitd.ui.components.view
 
 import android.view.View
-import androidx.recyclerview.widget.LinearLayoutManager
 import coil.load
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyHolder
 import com.airbnb.epoxy.EpoxyModelClass
 import com.airbnb.epoxy.EpoxyModelWithHolder
-import com.airbnb.epoxy.SimpleEpoxyController
 import dev.punitd.data.Article
 import dev.punitd.ui.components.R
 import dev.punitd.ui.components.databinding.ViewArticleItemBinding
-import dev.punitd.ui.components.insetOnlyCarouselPadding
 import dev.punitd.ui.components.view.ArticleItemView.ArticleItemHolder
 
 @EpoxyModelClass
@@ -19,9 +16,6 @@ abstract class ArticleItemView : EpoxyModelWithHolder<ArticleItemHolder>() {
 
     @EpoxyAttribute
     lateinit var article: Article
-
-    @EpoxyAttribute
-    lateinit var categories: List<CategoryView>
 
     override fun getDefaultLayout() = R.layout.view_article_item
 
@@ -36,8 +30,6 @@ abstract class ArticleItemView : EpoxyModelWithHolder<ArticleItemHolder>() {
             articleTitle.text = article.title
             authorName.text = article.author
             pubDate.text = article.pubDate
-
-            categoryCarousel.setModels(categories)
         }
     }
 
@@ -45,15 +37,6 @@ abstract class ArticleItemView : EpoxyModelWithHolder<ArticleItemHolder>() {
         lateinit var binding: ViewArticleItemBinding
         override fun bindView(itemView: View) {
             binding = ViewArticleItemBinding.bind(itemView)
-            binding.categoryCarousel.apply {
-                layoutManager = LinearLayoutManager(
-                    binding.root.context,
-                    LinearLayoutManager.HORIZONTAL,
-                    false
-                )
-                adapter = SimpleEpoxyController().adapter
-                setPadding(insetOnlyCarouselPadding())
-            }
         }
     }
 }
