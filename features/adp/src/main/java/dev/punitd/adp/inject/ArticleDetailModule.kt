69fab0c5d0d089151e7a356cc28cb6aa1e126e6b
punitda <punitdama@gmail.com>
2021-10-01 18:21:43 +0530

    Fix wildcardimport lint error

--- features/adp/src/main/java/dev/punitd/adp/inject/ArticleDetailModule.kt
+++ features/adp/src/main/java/dev/punitd/adp/inject/ArticleDetailModule.kt
@@ -4,7 +4,12 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import dev.punitd.adp.domain.usecase.*
+import dev.punitd.adp.domain.usecase.BookmarkArticleUseCase
+import dev.punitd.adp.domain.usecase.BookmarkArticleUseCaseImpl
+import dev.punitd.adp.domain.usecase.IsArticleBookmarkedUseCase
+import dev.punitd.adp.domain.usecase.IsArticleBookmarkedUseCaseImpl
+import dev.punitd.adp.domain.usecase.RemoveBookmarkedArticleUseCase
+import dev.punitd.adp.domain.usecase.RemoveBookmarkedArticleUseCaseImpl
 
 @InstallIn(ViewModelComponent::class)
 @Module
