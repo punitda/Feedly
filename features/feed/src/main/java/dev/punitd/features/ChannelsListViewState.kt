078a9062db9b843a84e3487486c8125f0ce96063
punitda <punitdama@gmail.com>
2021-10-02 20:42:07 +0530

    Remove unused view state class

--- features/feed/src/main/java/dev/punitd/features/ChannelsListViewState.kt
+++
@@ -1,9 +0,0 @@
-package dev.punitd.features
-
-import dev.punitd.data.Channel
-
-sealed class ChannelsListViewState {
-    object Initial : ChannelsListViewState()
-    data class Error(val message: String) : ChannelsListViewState()
-    data class Success(val channels: List<Channel>) : ChannelsListViewState()
-}
